3c4738343ced42a68c91012673a09b6c0fe80e84
Kirill <kushovk2003@mail.ru>
2025-05-02 21:58:26 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-3d",
+  base: "/portfolio-3d/",
   plugins: [react(), tailwindcss()],
 });
