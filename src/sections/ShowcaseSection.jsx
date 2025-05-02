8c0afb3949819827af3a5cabb579fa1fbc363d26
Kirill <kushovk2003@mail.ru>
2025-05-02 22:19:03 +0500

    fix

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -52,7 +52,7 @@ const ShowcaseSection = () => {
           {/* left content */}
           <div className="first-project-wrapper" ref={project1Ref}>
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde" />
+              <img src="images/project1.png" alt="Ryde" />
             </div>
             <div className="text-content">
               <h2>
@@ -70,14 +70,14 @@ const ShowcaseSection = () => {
           <div className="project-list-wrapper overflow-hidden">
             <div className="project" ref={project2Ref}>
               <div className="image-wrapper bg-[#ffefdb]">
-                <img src="/images/project2.png" alt="management platform" />
+                <img src="images/project2.png" alt="management platform" />
               </div>
               <h2>The Library Management Platform</h2>
             </div>
 
             <div className="project" ref={project3Ref}>
               <div className="image-wrapper bg-[#ffe7eb]">
-                <img src="/images/project3.png" alt="directory" />
+                <img src="images/project3.png" alt="directory" />
               </div>
               <h2>YC Directory - A Startup Showcase App</h2>
             </div>
